1df43398b2eb38522f855029ef3bdb5bc1e2f5bd
zurson <matu.jurek@gmail.com>
2025-01-14 11:04:50 +0100

    Added null value handling for gson LocalDate and LocalDateTime adapters

--- app/src/main/java/com/thesis/arrivo/communication/gson/LocalDateAdapter.kt
+++ app/src/main/java/com/thesis/arrivo/communication/gson/LocalDateAdapter.kt
@@ -1,7 +1,9 @@
 package com.thesis.arrivo.communication.gson
 
+import com.google.gson.JsonSyntaxException
 import com.google.gson.TypeAdapter
 import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonToken
 import com.google.gson.stream.JsonWriter
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -10,14 +12,23 @@ class LocalDateAdapter : TypeAdapter<LocalDate>() {
     private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
 
     override fun write(out: JsonWriter, value: LocalDate?) {
-        if (value == null) {
-            out.nullValue()
-        } else {
-            out.value(value.format(formatter))
-        }
+        out.value(value?.format(formatter))
     }
 
-    override fun read(input: JsonReader): LocalDate {
-        return LocalDate.parse(input.nextString(), formatter)
+    override fun read(input: JsonReader): LocalDate? {
+        return when (input.peek()) {
+            JsonToken.NULL -> {
+                input.nextNull()
+                null
+            }
+
+            JsonToken.STRING -> {
+                val dateString = input.nextString()
+                LocalDate.parse(dateString, formatter)
+            }
+
+            else -> throw JsonSyntaxException("Expected a string for LocalDate but was ${input.peek()}")
+        }
     }
 }
+
--- app/src/main/java/com/thesis/arrivo/communication/gson/LocalDateTimeAdapter.kt
+++ app/src/main/java/com/thesis/arrivo/communication/gson/LocalDateTimeAdapter.kt
@@ -1,7 +1,9 @@
 package com.thesis.arrivo.communication.gson
 
+import com.google.gson.JsonSyntaxException
 import com.google.gson.TypeAdapter
 import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonToken
 import com.google.gson.stream.JsonWriter
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -10,14 +12,21 @@ class LocalDateTimeAdapter : TypeAdapter<LocalDateTime>() {
     private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
 
     override fun write(out: JsonWriter, value: LocalDateTime?) {
-        if (value == null) {
-            out.nullValue()
-        } else {
-            out.value(value.format(formatter))
-        }
+        out.value(value?.format(formatter))
     }
 
-    override fun read(input: JsonReader): LocalDateTime {
-        return LocalDateTime.parse(input.nextString(), formatter)
+    override fun read(input: JsonReader): LocalDateTime? {
+        return when (input.peek()) {
+            JsonToken.NULL -> {
+                input.nextNull()
+                null
+            }
+            JsonToken.STRING -> {
+                val dateString = input.nextString()
+                LocalDateTime.parse(dateString, formatter)
+            }
+            else -> throw JsonSyntaxException("Expected a string for LocalDateTime but was ${input.peek()}")
+        }
     }
 }
+
