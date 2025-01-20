84b774ecc999cb6f9ee35baec50a276948319c32
zurson <matu.jurek@gmail.com>
2025-01-20 14:40:56 +0100

    JWT security setup

--- app/src/main/java/com/thesis/arrivo/view_models/MainScaffoldViewModel.kt
+++ app/src/main/java/com/thesis/arrivo/view_models/MainScaffoldViewModel.kt
@@ -1,8 +1,6 @@
 package com.thesis.arrivo.view_models
 
 import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -11,25 +9,31 @@ import com.google.firebase.auth.FirebaseAuth
 import com.thesis.arrivo.activities.MainActivity
 import com.thesis.arrivo.communication.employee.Employee
 import com.thesis.arrivo.communication.task.Task
-import com.thesis.arrivo.components.navigation.NavigationItem
 import com.thesis.arrivo.communication.task.TaskToEdit
+import com.thesis.arrivo.components.navigation.NavigationItem
 import com.thesis.arrivo.utilities.Location
 import com.thesis.arrivo.utilities.NavigationManager
-import com.thesis.arrivo.utilities.Settings.Companion.AUTH_ACCOUNT_STATUS_CHECK_INTERVAL_MS
 import com.thesis.arrivo.utilities.changeActivity
 import com.thesis.arrivo.utilities.interfaces.LoadingScreenManager
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
 
 class MainScaffoldViewModel(
     private val context: Context,
     private var adminMode: Boolean,
     private val navigationManager: NavigationManager
 ) : ViewModel(), LoadingScreenManager {
+
+    companion object {
+        fun reset() {
+            FirebaseAuth.getInstance().signOut()
+
+            changeActivity(
+                context = MainActivity.context,
+                destActivity = MainActivity::class,
+                finish = true
+            )
+        }
+    }
 
     /**
      * NavBar elements
@@ -75,11 +79,11 @@ class MainScaffoldViewModel(
 
     fun getStartDestination(callback: (NavigationItem) -> Unit) {
         isUserAuthenticated { isAuthenticated ->
-            val startDestination = if (!isAuthenticated) {
-                NavigationItem.Login
-            } else {
-                getNavbarElements().first()
+            val startDestination = when (isAuthenticated) {
+                true -> getNavbarElements().first()
+                false -> NavigationItem.Login
             }
+
             callback(startDestination)
         }
     }
@@ -132,50 +136,32 @@ class MainScaffoldViewModel(
     private var accountBlockJob: Job? = null
 
 
-    private fun isConnectedToInternet(): Boolean {
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-        val activeNetwork = connectivityManager.activeNetwork
-        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
-
-        return capabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
-    }
-
-
-    private fun reset() {
-        accountBlockJob?.cancel()
-
-        changeActivity(
-            context = context,
-            destActivity = MainActivity::class,
-            finish = true
-        )
-    }
-
-
     private fun listenAuthStatus() {
-        if (accountBlockJob?.isActive == true)
-            return
+        FirebaseAuth.AuthStateListener {
 
-        accountBlockJob = CoroutineScope(Dispatchers.IO).launch {
-            while (isActive) {
-                delay(AUTH_ACCOUNT_STATUS_CHECK_INTERVAL_MS)
-
-                if (FirebaseAuth.getInstance().currentUser == null || !isConnectedToInternet())
-                    continue
-
-                isUserAuthenticated { isAuthenticated ->
-                    if (!isAuthenticated)
-                        reset()
-                }
-            }
         }
+
+//        if (accountBlockJob?.isActive == true)
+//            return
+//
+//        accountBlockJob = CoroutineScope(Dispatchers.IO).launch {
+//            while (isActive) {
+//                delay(AUTH_ACCOUNT_STATUS_CHECK_INTERVAL_MS)
+//
+//                if (FirebaseAuth.getInstance().currentUser == null || !isConnectedToInternet())
+//                    continue
+//
+//                isUserAuthenticated { isAuthenticated ->
+//                    if (!isAuthenticated)
+//                        reset()
+//                }
+//            }
+//        }
     }
 
 
     fun startAuthListeners() {
-        listenAuthStatus()
+//        listenAuthStatus()
     }
 
 
